3eb22e36c3e2e5b22b8a3b3aa0eee8c59c7ca3d3
Chris <chrismcvickar@gmail.com>
2023-07-29 23:55:59 -0700

    add mod

---
+++ src/main/java/com/lotkey/villagerflesh/VillagerFlesh.java
@@ -0,0 +1,66 @@
+package com.lotkey.villagerflesh;
+
+import com.lotkey.villagerflesh.item.ModItems;
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(VillagerFlesh.MOD_ID)
+public class VillagerFlesh {
+    // Define mod id in a common place for everything to reference
+    public static final String MOD_ID = "villagerflesh";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public VillagerFlesh() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+
+        // Register the item to a creative tab
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    private void addCreative(CreativeModeTabEvent.BuildContents event) {
+        ModItems.setCreativeTabs(event);
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods
+    // in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+}
---
+++ src/main/java/com/lotkey/villagerflesh/item/ModItems.java
@@ -0,0 +1,45 @@
+package com.lotkey.villagerflesh.item;
+
+import com.lotkey.villagerflesh.VillagerFlesh;
+
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
+            VillagerFlesh.MOD_ID);
+
+    public static final RegistryObject<Item> VILLAGER_FLESH = ITEMS.register(
+            "villager_flesh", () -> new Item(new Item.Properties().food(Foods.VILLAGER_FLESH)));
+    public static final RegistryObject<Item> COOKED_VILLAGER_FLESH = ITEMS.register(
+            "cooked_villager_flesh", () -> new Item(new Item.Properties().food(Foods.COOKED_VILLAGER_FLESH)));
+
+    public static class Foods {
+        public static final FoodProperties VILLAGER_FLESH = new FoodProperties.Builder().nutrition(3)
+                .saturationMod(0.3F).meat()
+                .effect(() -> new MobEffectInstance(MobEffects.BAD_OMEN, -1, 1), 0.05f)
+                .build();
+        public static final FoodProperties COOKED_VILLAGER_FLESH = new FoodProperties.Builder().nutrition(8)
+                .saturationMod(0.8F).meat()
+                .effect(() -> new MobEffectInstance(MobEffects.BAD_OMEN, -1, 1), 0.025f).build();
+    }
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+    public static void setCreativeTabs(CreativeModeTabEvent.BuildContents event) {
+        if (event.getTab() == CreativeModeTabs.FOOD_AND_DRINKS) {
+            event.accept(VILLAGER_FLESH);
+            event.accept(COOKED_VILLAGER_FLESH);
+        }
+    }
+}
